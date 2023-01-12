a780f05e676ba61e9afb0b8fa55f3111f30487b4
Haque360 <shahilyasarhaque@gmail.com>
2023-01-12 18:35:22 +0600

    firebase connection problem fixed

--- src/components/AddPost.js
+++ src/components/AddPost.js
@@ -1,11 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { View, TextInput, Button, Modal,StyleSheet ,Text} from 'react-native';
 import { Input, Card } from "@rneui/themed";
+import { getAuth } from 'firebase/auth';
 // import * as firebase from 'firebase';
+import { doc, setDoc } from "firebase/firestore"; 
+import { getDatabase , ref, set} from "firebase/database";
+
+const database = getDatabase();
+
+
 
 const AddPost = ({ visible, setVisible }) => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const [postId, setPostId] = React.useState('');;
+  const [email,setEmail] = useState()
+
+  useEffect(() => {
+    const auth= getAuth();
+    setEmail(auth.currentUser.email)
+    // firebase.database().ref(`users/${userId}`).on('value', snapshot => {
+    //     setUser(snapshot.val());
+    // });
+}, [email]);
+
+  const auth=getAuth()
+
+  function writeUserData(title,content,postID, userEmail) {
+    const db = getDatabase();
+    set(ref(db, 'posts'), {
+      title: setTitle(''),
+      content: setContent(''),
+      postID:setPostId(''),
+      email: setEmail(''),
+    });
+  }
+
+  const generateRandomString = (lenth) => {
+  const char = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
+  const random = Array.from(
+      {length: lenth},
+      () => char[Math.floor(Math.random() * char.length)]
+  );
+  const randomString = random.join("");
+  return setPostId(randomString);
+  }
 
   const handleAddPost = () => {
     // const userId = firebase.auth().currentUser.uid;
@@ -13,14 +52,16 @@ const AddPost = ({ visible, setVisible }) => {
     //   title,
     //   content
     // });
+    generateRandomString(10)
     setTitle('');
     setContent('');
     setVisible(false);
+    writeUserData();
   };
 
   return (
     <Modal animationType="slide" visible={visible}>
-      <View>
+      <View style={styles.viewStyle}>
         <Card>
             <Text styles={styles.titleStyles}>Add Post</Text>
             <Card.Divider />
@@ -44,19 +85,21 @@ const AddPost = ({ visible, setVisible }) => {
             <Card.Divider />
             <View style={styles.buttonContainer}>
               <Button styles={styles.button} title="Add Post" onPress={handleAddPost} />
+              <Text> </Text>
               <Button styles={styles.button} title="Cancel" onPress={() => setVisible(false)} />
             </View>
         </Card>
-      
       </View>
     </Modal>
   );
 };
 
 const styles = StyleSheet.create({
+  viewStyle: {
+    flex: 1,
+  },
     container:{
      justifyContent:'center',
-     alignItems:'center'
     }
     ,
    buttonContainer:{
