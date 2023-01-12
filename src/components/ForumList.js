ef5880287fd8a9731fbb5ba42e3a24fcfbdddfc0
Haque360 <shahilyasarhaque@gmail.com>
2023-01-12 21:55:43 +0600

    forum are visible in forum list

--- src/components/ForumList.js
+++ src/components/ForumList.js
@@ -1,14 +1,70 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { Component ,useEffect,useState} from 'react';
+import { Input, Button, Card } from "@rneui/themed";
+import { View, Text, FlatList, TouchableOpacity,StyleSheet} from 'react-native';
+import { collection, query, where, getDocs } from "firebase/firestore"; 
+import { db } from '../../firebase';
+import * as firebase from "firebase/app";
+
 
 const ForumList = () => {
-  return (
+  const[post,setPosts]=useState([])
+
+  useEffect(() => {
+    const li=[]
+    const loadData = async () => {
+      try{
+        const querySnapshot = await getDocs(collection(db, "posts"));
+        querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          console.log(doc.id, " => ", doc.data());
+          li.push(doc.data())
+         });
+         setPosts(li)
+      }
+      catch(error){
+        alert(error.message)
+      }
+        
+    }
+    loadData()
+   }, [setPosts]);
+
+
+  //  async function readData() 
+  // {
+  //   setPosts([])
+  //   const allPosts=[]
+  //   const querySnapshot =  getDocs(collection(db, "posts"));
+  //   querySnapshot.forEach((post) => {
+  //   {allPosts.push({key: post.id, name: post.data().name} )}
+  //   });
+  //   setPosts(allPosts)
+  // }
+  const handlePress = (item) => {
+    console.log('Pressed', item);
+  }
+
+
+  return(
     <View>
-      <Text>ForumList</Text>
+      <FlatList
+      data={post}
+      renderItem={
+        ({item})=>
+        <TouchableOpacity onPress={() => handlePress(item)}>
+          <Card>
+            <Text style={styles.textStyle}>{item.title}</Text>
+         </Card>
+        </TouchableOpacity>
+      }
+      />
     </View>
   )
 }
+export default ForumList;
 
-export default ForumList
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 20,
+  }
+  });
